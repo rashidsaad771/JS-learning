934d6f17dda779409a5fc282ac553c7fb90379be
Rashid Saad <149606262+rashidsaad771@users.noreply.github.com>
2024-05-22 04:31:58 +0000

    data types of js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat code as a newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("rashid")
+
+let name = "rashid"
+let age = 25
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
