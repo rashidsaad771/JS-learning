03ecd2557f7a5b56ddcf53b372bfd6ba1bc414a5
Rashid Saad <149606262+rashidsaad771@users.noreply.github.com>
2024-05-22 17:21:14 +0000

    date and time

---
+++ 01_basics/07_datesInJS.js
@@ -0,0 +1,33 @@
+// Dates
+
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+
+
+// let myCreatedDate = new Date(2024, 0, 22)
+// let myCreatedDate = new Date(2024, 0, 22, 18, 20)
+let myCreatedDate = new Date("2024-01-23")
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+// console.log(Math.floor(myTimeStamp / 1000))
+
+
+let newDate = new Date()
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getHours());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getMonth());
