f8abe307d50d1c1a0e3c23e1b84bd0a966de93dd
Rashid Saad <149606262+rashidsaad771@users.noreply.github.com>
2024-05-24 11:43:33 +0000

    functions with objects and arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,4 +32,34 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in.`
 }
 
-console.log(loginUserMessage("rashid"));
+// console.log(loginUserMessage("rashid"));
+
+
+function calcualteCartPrice(...num1){
+    return num1
+}
+
+// console.log(calcualteCartPrice(200, 300, 500, 2000));
+
+const user = {
+    name: "rashid",
+    price: 199,
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.name} and price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    name: "Sam",
+    price: 399,
+})
+
+const myArray = [1000, 250, 200, 500]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myArray))
+console.log(returnSecondValue([200, 30, 500, 600]))
